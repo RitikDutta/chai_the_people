24b48ba83325b13b1027d208295896cf6617003f
ritik dutta <ritikduttagd@gmail.com>
2023-03-20 23:22:29 +0530

    added different routes

--- app.py
+++ app.py
@@ -1,25 +1,111 @@
 from flask import Flask, request, render_template
-
+from database_operations import CassandraCRUD
+import datetime
 app = Flask(__name__)
+
+
+
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+@app.route('/add_question', methods=['GET', 'POST'])
+def add_question():
+    options=''
+    crud = CassandraCRUD("test_key")
+    question = request.form.get('question')
+    options = request.form.get('options')
+    # txt = question.upper()
+    print("questio ", question)
+    print("options", options)
+    if question is not None:
+        if options is not None:
+            crud.insert_survey_data(max(list(map(max, crud.last("survey").values)))+1, datetime.datetime.now().date(),  question, options)
+
+    return render_template('add_question.html')
+
+
+
+
+
+
+@app.route('/add_user', methods=['GET', 'POST'])
+def add_user():
+    crud = CassandraCRUD("test_key")
+    args = request.args
+    user_id = str(args.get("id"))
+    
+    name = request.form.get('name')
+    age = request.form.get('age')
+    annual_income = request.form.get('annual_income')
+    city = request.form.get('city')
+    gender = request.form.get('gender')
+    marital_status = request.form.get('marital status')
+    qualification = request.form.get('qualification')
+    occupation = request.form.get('occupation')
+
+    # txt = question.upper()
+    print("name ", name)
+    print("age ", age)
+    print("annual_income ", annual_income)
+    print("city ", city)
+    print("gender ", gender)
+    print("marital_status ", marital_status)
+    print("qualification ", qualification)
+    print('occupation ', occupation)
+    if not (name ==None or age ==None or annual_income ==None or city ==None or gender ==None or marital_status ==None or qualification ==None or occupation ==None):
+        crud.insert_user_data(user_id, datetime.datetime.now().date(), name, gender, age, annual_income, city, marital_status, qualification)
+        print("No NONE Values")
+    else:
+        print("NONE VALUES")
+    return render_template('add_user.html')
+
+
+
+
+
+
 
 @app.route('/questions', methods=['GET', 'POST'])
-def questions():
-    questions = {
-        1: {'text': 'What is your name?', 'options': ['John', 'Mary', 'Tom']},
-        2: {'text': 'What is your age?', 'options': ['18-25', '26-35']},
-        3: {'text': 'What is your favorite color?', 'options': ['Red', 'Green', 'Blue']}
-    }
+def survey():
+    crud = CassandraCRUD("test_key")
+    dictionary_fields = {'text': ' ', 'options': ' '}
+    db = db = crud.get_db("survey")
+    
+    question_id = []
+    for i in range(len(db)):
+        question_id.append(i+1)
+    question_id
+
+    student_record = {}
+    #To create 5 records: We will run loop for 5 times
+    for i in range(0, len(db)):
+        student_record.update({question_id[i]: dict(dictionary_fields)})
+    #Add two items to 2D dictionary
+    for i in range(1, len(db)+1):
+        options = db.iloc[i-1].options.split(",")
+        student_record[i]['text'] = db.iloc[i-1].question
+        student_record[i]['options'] = options
+
+    # for i in range(1, 3):
+    #     print(student_record[i])
+        
+    student_record
+    questions = student_record
     args = request.args
-    print(args.get("id"))
+    uid = args.get("id")
+    # chk = args.get("chk")
+    print(uid)
+    # print(chk)
+    # if args.get("chk") == "no":
+
 
     if request.method == 'POST':
         answers = {}
@@ -27,8 +113,21 @@ def questions():
             answers[question_id] = request.form.get(str(question_id))
         # return str(answers)
         print(answers)
+        print(type(answers))
+
+        if not (uid == None or question_id == None):
+            for i in range(len(answers)):
+                if not answers[i+1]==None:
+                    crud.insert_response_data(uid, i, answers[i+1], datetime.datetime.now().date())
+
+
     return render_template('questions.html', questions=questions)
 
+    # else:
+        # return render_template('add_user.html', uid=uid)
 
-if __name__ == "__main__":
+
+
+
+if __name__ == '__main__':
     app.run(debug=True)
